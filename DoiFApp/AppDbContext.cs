83feb9dc6a91cf8b611c120bf6fad809db34aecc
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-21 11:46:01 +0300

    db

---
+++ DoiFApp/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DoIFToolApp.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoiFApp
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<LessonModel> Lessons { get; set; }
+
+        public void ClearDb()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=doifapp.db");
+        }
+    }
+}
