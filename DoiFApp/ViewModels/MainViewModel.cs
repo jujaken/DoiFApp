7ac9bffa8808d5d6d0146c9328d11ef701b45149
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-21 09:24:49 +0300

    tool logic

--- DoiFApp/ViewModels/MainViewModel.cs
+++ DoiFApp/ViewModels/MainViewModel.cs
@@ -1,41 +1,84 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace DoiFApp.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
         [ObservableProperty]
-        private NotifyViewModel? currentNotify = new NotifyViewModel();
+        private ObservableCollection<ToolViewModel> commands = [];
+
+
+        public MainViewModel()
+        {
+            commands.Add(new ToolViewModel()
+            {
+                Title = "Загрузи эксель",
+                Description = "Загружает таблицу excel и формирует необходимые данные для работы приложения",
+                Command = LoadExcelCommand
+            });
+
+            commands.Add(new ToolViewModel()
+            {
+                Title = "Выгрузи во временный файл",
+                Description = "Выгружает таблицу excel в temp таблицу",
+                Command = ExtractToTempFileCommand
+            });
+
+            commands.Add(new ToolViewModel()
+            {
+                Title = "Загрузи temp-файл",
+                Description = "Загружает temp-файл для для просмотра",
+                Command = LoadTempFileCommand
+            });
+
+            commands.Add(new ToolViewModel()
+            {
+                Title = "Сформируй загруженность",
+                Description = "Формирует таблицу загруженности",
+                Command = ExctractWorkloadTableCommand
+            });
+
+            commands.Add(new ToolViewModel()
+            {
+                Title = "Сформируй отчёт",
+                Description = "Формирует таблицу-отчёт",
+                Command = ExctractReportTableCommand
+            });
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<NotifyViewModel> notifies = [];
 
         [RelayCommand]
         public async Task LoadExcel()
         {
-            
+            throw new NotImplementedException();
         }
 
         [RelayCommand]
         public async Task ExtractToTempFile()
         {
-
+            throw new NotImplementedException();
         }
 
         [RelayCommand]
         public async Task LoadTempFile()
         {
-
+            throw new NotImplementedException();
         }
 
         [RelayCommand]
         public async Task ExctractWorkloadTable()
         {
-
+            throw new NotImplementedException();
         }
 
         [RelayCommand]
         public async Task ExctractReportTable()
         {
-
+            throw new NotImplementedException();
         }
     }
 }
---
+++ DoiFApp/ViewModels/ToolViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace DoiFApp.ViewModels
+{
+    public partial class ToolViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = "tool title";
+
+        [ObservableProperty]
+        private string description = "tool desc";
+
+        public ICommand? Command { get; set; }
+    }
+}
