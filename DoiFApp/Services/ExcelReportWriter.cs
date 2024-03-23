304028eff28ab01816bbf494dced09ef2cd99afe
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-23 10:47:53 +0300

    def

---
+++ DoiFApp/Services/ExcelReportWriter.cs
@@ -0,0 +1,26 @@
+﻿using DoiFApp.Data.Models;
+using DoiFApp.Data.Repo;
+using OfficeOpenXml;
+using System.IO;
+
+namespace DoiFApp.Services
+{
+    public class ExcelReportWriter(IRepo<LessonModel> lessonRepo) : IReportWriter
+    {
+        private readonly IRepo<LessonModel> lessonRepo = lessonRepo;
+
+        public async Task Write(string path)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+
+            using var package = new ExcelPackage(path);
+            var data = package.Workbook.Worksheets.Add("Таблица по месяцам");
+
+            (await lessonRepo.GetAll()).ForEach(l =>
+            {
+
+            });
+        }
+    }
+}
---
+++ DoiFApp/Services/IReportWriter.cs
@@ -0,0 +1,7 @@
+﻿namespace DoiFApp.Services
+{
+    public interface IReportWriter
+    {
+        Task Write(string path);
+    }
+}
