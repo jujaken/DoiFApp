65b4a4c2efded9b039ee4b746a7de2ab92180f5f
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-09-26 16:51:14 +0300

    save

--- DoiFApp/Services/Word/WordIndividualPlanWriter.cs
+++ DoiFApp/Services/Word/WordIndividualPlanWriter.cs
@@ -1,5 +1,6 @@
 ﻿using DoiFApp.Data.Models;
 using DoiFApp.Utils;
+using System.Windows.Forms;
 using Xceed.Document.NET;
 using Xceed.Words.NET;
 
@@ -19,60 +20,78 @@ namespace DoiFApp.Services.Word
         private static void UpdateTables(EducationTeacherModel teacher, List<Table> tables)
         {
             // ПЛАНИРУЕМАЯ НА 1 ПОЛУГОДИЕ
-            InsertData(tables[0], teacher.Works1);
+            var w1Dones = InsertData(tables[0], teacher.Works1);
             // ПЛАНИРУЕМАЯ НА 2 ПОЛУГОДИЕ
-
+            InsertData(tables[1], teacher.Works2, w1Dones);
             // ФАКТИЧЕСКИ ВЫПОЛНЕННАЯ В 1 ПОЛУГОДИИ
-
             // ФАКТИЧЕСКИ ВЫПОЛНЕННАЯ В 2 ПОЛУГОДИИ
-
             // ЕЖЕМЕСЯЧНЫЙ УЧЁТ ВЫПОЛНЕНИЯ УЧЕБНОЙ НАГРУЗКИ 
 
         }
 
-        private static void InsertData(Table table, List<EducationWorkModel> works)
+        private static double[] InsertData(Table table, List<EducationWorkModel> works, double[]? lastDones = null)
         {
-            var doneList = new double[50];
+            for (int i = table.Rows.Count - (lastDones == null ? 2 : 3); i < works.Count; i++)
+                table.InsertRow(1);
+
+            var dones = new double[50];
+            var ri = 0;
             foreach (var work in works)
             {
-                var row = table.InsertRow(1);
+                var row = table.Rows[++ri];
                 row.Cells[0].Paragraphs[0].Append(work.Name);
                 for (int i = 1; i < row.Cells.Count - 2; i++)
                 {
                     var cell = row.Cells[i];
                     var item = work.TypesAndHours[i - 1];
                     var value = item.Value.ToString("0.0", System.Globalization.CultureInfo.GetCultureInfo("en-US")) ?? "0.0";
-                    doneList[i - 1] += work.TypesAndHours[i - 1].Value;
+                    dones[i - 1] += work.TypesAndHours[i - 1].Value;
                     cell.Paragraphs[0].Append(value);
                 }
 
                 var workSum = work.TypesAndHours
                     .Sum(x => x.Value);
-                
-                doneList[row.Cells.Count - 3] = workSum;
+
+                dones[row.Cells.Count - 3] = workSum;
                 row.Cells[^2].Paragraphs[0].Append(workSum.ToString("0.0", System.Globalization.CultureInfo.GetCultureInfo("en-US")));
 
                 var audiWorkSum = work.TypesAndHours
                     .Where(x => TableDataUtil.GetEquivalent(x.Key) != null)
                     .Sum(x => x.Value);
 
-                doneList[row.Cells.Count - 2] = workSum;
+                dones[row.Cells.Count - 2] = workSum;
                 row.Cells[^1].Paragraphs[0].Append(audiWorkSum.ToString("0.0", System.Globalization.CultureInfo.GetCultureInfo("en-US")));
             }
 
-            var lastRow = table.Rows[^1];
+            var lastRow = table.Rows[^(lastDones == null ? 1 : 2)];
             for (int i = 1; i < lastRow.Cells.Count; i++)
             {
                 var cell = lastRow.Cells[i];
                 cell.Paragraphs[0].ReplaceText(new StringReplaceTextOptions()
                 {
                     SearchValue = "0.0",
-                    NewValue = doneList[i - 1].ToString("0.0",
+                    NewValue = dones[i - 1].ToString("0.0",
                         System.Globalization.CultureInfo.GetCultureInfo("en-US")),
                 });
             }
 
+            if (lastDones != null)
+            {
+                var yearRow = table.Rows[^1];
+                for (int i = 1; i < lastRow.Cells.Count; i++)
+                {
+                    var cell = lastRow.Cells[i];
+                    cell.Paragraphs[0].ReplaceText(new StringReplaceTextOptions()
+                    {
+                        SearchValue = "0.0",
+                        NewValue = (dones[i - 1] + lastDones[i - 1]).ToString("0.0",
+                            System.Globalization.CultureInfo.GetCultureInfo("en-US")),
+                    });
+                }
+            }
+
             table.Design = TableDesign.TableGrid;
+            return dones;
         }
     }
 }
