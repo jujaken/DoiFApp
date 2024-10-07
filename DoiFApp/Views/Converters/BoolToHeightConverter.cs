88ef8c4a2b362307fdacb52b564e8b982a3a1694
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-10-07 10:17:19 +0300

    category

---
+++ DoiFApp/Views/Converters/BoolToHeightConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DoiFApp.Views.Converters
+{
+    public class BoolToHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return boolValue ? double.NaN : 0;
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
