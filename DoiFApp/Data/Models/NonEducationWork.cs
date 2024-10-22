6bdd213330c88cf3936701f554789d609b32a00d
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-10-22 11:16:10 +0300

    add noneducationwork

---
+++ DoiFApp/Data/Models/NonEducationWork.cs
@@ -0,0 +1,27 @@
+﻿namespace DoiFApp.Data.Models
+{
+    public class NonEducationWork : Model
+    {
+        public required string Text { get; set; }
+        public NonEducationWorkType Type { get; set; }
+        public NonEducationWorkSemesterType Semester { get; set; }
+
+    }
+
+    public enum NonEducationWorkSemesterType
+    {
+         None,
+         First,
+         Second,
+    }
+
+    public enum NonEducationWorkType
+    {
+        None,
+        Methodic,
+        Scientic,
+        Educational,
+        Foreignic,
+        Other,
+    }
+}
