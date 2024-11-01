d1d03d0ec0f97b2d893b379cfad0a2ad568c73cf
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-11-01 16:08:15 +0300

    false

--- DoiFApp/ViewModels/Pages/ExtractWorkloadPageViewModel.cs
+++ DoiFApp/ViewModels/Pages/ExtractWorkloadPageViewModel.cs
@@ -7,18 +7,18 @@ namespace DoiFApp.ViewModels.Pages
     {
         [ObservableProperty]
         private MonthViewModel[] months = [
-            new () { Id = 9, Name = "Сентябрь", IsSelected = true },
-            new () { Id = 10, Name = "Октябрь", IsSelected = true },
-            new () { Id = 11, Name = "Ноябрь", IsSelected = true },
-            new () { Id = 12, Name = "Декабрь", IsSelected = true },
-            new () { Id = 1, Name = "Январь", IsSelected = true },
-            new () { Id = 2, Name = "Февраль", IsSelected = true },
-            new () { Id = 3, Name = "Март", IsSelected = true },
-            new () { Id = 4, Name = "Апрель", IsSelected = true },
-            new () { Id = 5, Name = "Май", IsSelected = true },
-            new () { Id = 6, Name = "Июнь", IsSelected = true },
-            new () { Id = 7, Name = "Июль", IsSelected = true },
-            new () { Id = 8, Name = "Август", IsSelected = true },
+            new () { Id = 9, Name = "Сентябрь", IsSelected = false },
+            new () { Id = 10, Name = "Октябрь", IsSelected = false },
+            new () { Id = 11, Name = "Ноябрь", IsSelected = false },
+            new () { Id = 12, Name = "Декабрь", IsSelected = false },
+            new () { Id = 1, Name = "Январь", IsSelected = false },
+            new () { Id = 2, Name = "Февраль", IsSelected = false },
+            new () { Id = 3, Name = "Март", IsSelected = false },
+            new () { Id = 4, Name = "Апрель", IsSelected = false },
+            new () { Id = 5, Name = "Май", IsSelected = false },
+            new () { Id = 6, Name = "Июнь", IsSelected = false },
+            new () { Id = 7, Name = "Июль", IsSelected = false },
+            new () { Id = 8, Name = "Август", IsSelected = false },
             ];
 
         [RelayCommand]
