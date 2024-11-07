33f66bbcb4023d0796754cbe185d7fb0fe4ae2e7
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-11-07 13:04:31 +0300

    для настройки нагрузки

--- DoiFApp/Services/NonEducationWork/NonEducationWorkData.cs
+++ DoiFApp/Services/NonEducationWork/NonEducationWorkData.cs
@@ -6,6 +6,8 @@ namespace DoiFApp.Services.NonEducationWork
     {
         public bool IsRewrite { get; init; } =  true;
 
+        public bool isCombine { get; init; } = false;
+
         public IEnumerable<DoiFApp.Data.Models.NonEducationWork>? NonEducationWorks { get; init; }
 
         public bool IsHolistic => NonEducationWorks != null && NonEducationWorks.Any();
