3a1f1478c0871b9da05eeb1de35e5c1e08dc9704
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-11-14 11:21:13 +0300

    fix

--- DoiFApp/Services/IndividualPlan/WordAbstractIndividualPlanWriter.cs
+++ DoiFApp/Services/IndividualPlan/WordAbstractIndividualPlanWriter.cs
@@ -26,11 +26,15 @@ namespace DoiFApp.Services.IndividualPlan
 
         protected abstract Task UpdateTables(EducationTeacherModel teacher, List<Table> tables);
 
-        protected async Task InsertData(Table table, List<EducationWorkModel> works)
+        protected async Task InsertData(Table table, List<EducationWorkModel> works, bool isSecond = false)
         {
             var activeWorks = works.Where(w => w.TypesAndHours.Sum(t => t.Value) != 0);
-            for (int i = table.Rows.Count - 2; i < activeWorks.Count(); i++)
+
+            for (int i = table.Rows.Count; i < activeWorks.Count() + 2 + (isSecond ? 1 : 0); i++)
                 table.InsertRow(1);
+
+            for (int i = table.Rows.Count; i > activeWorks.Count() + 2 + (isSecond ? 1 : 0); i--)
+                table.RemoveRow(1);
 
             var dones = new double[50];
             var ri = 0;
@@ -65,14 +69,17 @@ namespace DoiFApp.Services.IndividualPlan
                 row.Cells[^1].Paragraphs[0].Append(audiWorkSum.ToString("0.0", System.Globalization.CultureInfo.GetCultureInfo("en-US")));
             }
 
-            await InsertDones(table, dones);
+            if (isSecond)
+                await InsertDones(table, dones, 2);
+            else
+                await InsertDones(table, dones);
 
             table.Design = TableDesign.TableGrid;
         }
 
-        protected Task InsertDones(Table tableCurrent, double[] dones)
+        protected Task InsertDones(Table tableCurrent, double[] dones, int endIndex = 1)
         {
-            var resultRow = tableCurrent.Rows[^1];
+            var resultRow = tableCurrent.Rows[^endIndex];
             for (int i = 1; i < resultRow.Cells.Count; i++)
             {
                 var cell = resultRow.Cells[i];
@@ -85,7 +92,7 @@ namespace DoiFApp.Services.IndividualPlan
         protected Task InsertDones(Table tableCurrent, Table tableFrom)
         {
             var yearRow = tableCurrent.Rows[^1];
-            var donesCurrent = GetDonesFromTable(tableFrom, 2);
+            var donesCurrent = GetDonesFromTable(tableCurrent, 2);
             var donesFrom = GetDonesFromTable(tableFrom, 1);
             for (int i = 1; i < yearRow.Cells.Count; i++)
             {
@@ -98,10 +105,10 @@ namespace DoiFApp.Services.IndividualPlan
 
         protected double[] GetDonesFromTable(Table table, int endIndex)
         {
-            var donesRow = table.Rows[^(endIndex)];
-            var dones = new double[donesRow.Cells.Count];
-            for (int i = 1; i < dones.Length; i++)
-                dones[0] = Convert.ToDouble(donesRow.Cells[i].Paragraphs[0].Text, System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+            var donesRow = table.Rows[^endIndex];
+            var dones = new double[donesRow.Cells.Count - 1];
+            for (int i = 0; i < dones.Length; i++)
+                dones[i] = Convert.ToDouble(donesRow.Cells[i + 1].Paragraphs[0].Text, System.Globalization.CultureInfo.GetCultureInfo("en-US"));
             return dones;
         }
     }
