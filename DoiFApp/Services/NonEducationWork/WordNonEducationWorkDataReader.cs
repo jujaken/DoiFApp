fef8fe4baa12228d5e671165344376b8e621aaff
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-11-14 12:47:03 +0300

    add new

--- DoiFApp/Services/NonEducationWork/WordNonEducationWorkDataReader.cs
+++ DoiFApp/Services/NonEducationWork/WordNonEducationWorkDataReader.cs
@@ -5,17 +5,46 @@ namespace DoiFApp.Services.NonEducationWork
 {
     public class WordNonEducationWorkDataReader : IDataReader<NonEducationWorkData>
     {
-        // разработка или переработка в зависимости от выбора пользователя
-        // методическая
         public Task<NonEducationWorkData> Read(string path)
         {
             using var doc = DocX.Load(path);
+
             var educationWork = new List<DoiFApp.Data.Models.NonEducationWork>();
-            foreach (var paragrath in doc.Paragraphs)
+
+            var prefix = string.Empty;
+            var lastIsPrefix = false;
+            var lastPrefixIsMetodic = doc.Paragraphs[0].Text.Contains("2.1. Разработка и переработка учебно-методических материалов");
+            var paragraths = lastPrefixIsMetodic ? doc.Paragraphs.Skip(1) : doc.Paragraphs;
+            foreach (var paragrath in paragraths)
             {
                 var text = paragrath.Text;
                 if (string.IsNullOrEmpty(text)) continue;
-                educationWork.Add(new() { Text = text });
+                if (int.TryParse(text[0].ToString(), out var num))
+                {
+                    if (lastIsPrefix)
+                        educationWork.Add(new() { Text = prefix.Trim(':', ' ') });
+
+                    prefix = string.Empty;
+                    foreach (var part in text.Split(' ')[1..])
+                        prefix += part + ' ';
+                    prefix = prefix.Trim('.', ' ', ':', ';');
+                    prefix += ": ";
+
+                    lastIsPrefix = true;
+                    lastPrefixIsMetodic = false;
+                    continue;
+                }
+                else
+                    lastIsPrefix = false;
+
+                if (lastPrefixIsMetodic)
+                {
+                    educationWork.Add(new() { Text = "Разработка " + text[0].ToString().ToLower() + text[1..] });
+                    educationWork.Add(new() { Text = "Переработка " + text[0].ToString().ToLower() + text[1..] });
+                    continue;
+                }
+
+                educationWork.Add(new() { Text = prefix + text[0].ToString().ToLower() + text[1 ..] });
             }
             return Task.FromResult(new NonEducationWorkData() { NonEducationWorks = educationWork });
         }
