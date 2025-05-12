988a91c653f39319078f3c737c3ef4d6ff004157
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2025-05-12 10:14:01 +0300

    fix!

--- DoiFApp/Views/Controlls/AppSettingsPage.xaml.cs
+++ DoiFApp/Views/Controlls/AppSettingsPage.xaml.cs
@@ -1,24 +1,8 @@
-﻿using ag.WPF.ColorPicker;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using DoiFApp.ViewModels;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DoiFApp.Views.Controlls
 {
-    /// <summary>
-    /// Логика взаимодействия для AppSettingsPage.xaml
-    /// </summary>
     public partial class AppSettingsPage : UserControl
     {
         public AppSettingsPage()
@@ -26,9 +10,9 @@ namespace DoiFApp.Views.Controlls
             InitializeComponent();
         }
 
-        private void OpenColorPicker(object sender, RoutedEventArgs e)
+        private void ColorPicker_SelectedColorChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<System.Windows.Media.Color> e)
         {
-            var colorPanel = new ColorPanel();
+            (DataContext as ConfigColorViewModel)!.Value = e.NewValue;
         }
     }
 }
